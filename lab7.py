81270f2a975d6a9411fd3544846a769e04ea7a08
tori <vika.silanteva.05@list.ru>
2024-03-12 13:27:48 +0300

    lab7

--- lab7.py
+++ lab7.py
@@ -6,14 +6,14 @@ import time
 from mpl_toolkits.mplot3d import Axes3D
 from matplotlib.animation import FuncAnimation, PillowWriter
 
+
 def task1():
-    arr=[randint(0,100) for _ in range(10**6)]
-    arr2=[randint(0,100) for _ in range(10**6)]
+    arr = [randint(0,100) for _ in range(10**6)]
+    arr2 = [randint(0,100) for _ in range(10**6)]
     time.start = time.perf_counter()
-    result = [arr[i]*arr2[i] for i in range(10**6)]
+    result = [arr[i] * arr2[i] for i in range(10**6)]
     time.end = time.perf_counter()
     print("время выполнения операции поэлементного перемножения стандартных списков", time.end-time.start)
-
     time.start = time.perf_counter()
     result = np.multiply(np.array(arr),np.array(arr2))
     time.end = time.perf_counter()
@@ -24,29 +24,30 @@ def task1():
 def task2():
     arr = pd.read_csv("data2.csv")
     plt.figure(1)
-    plt.hist(arr["Solids"], bins=20)
+    plt.hist(arr["Solids"], bins = 20)
     plt.title("histogram")
     plt.xlabel("Solids")
     plt.ylabel("quantity")
     #plt.show()
 
     plt.figure(2)
-    plt.hist(arr["Solids"], bins=20, color="green", density=True)
+    plt.hist(arr["Solids"], bins = 20, color = "green", density = True)
     plt.title("histogram2")
     plt.xlabel("Solids")
     plt.ylabel("quantity")
-    plt.show()
+    #plt.show()
 
     print(round(arr["Solids"].std(), 2), "среднеквадратичное отклонение")
+
 
 def task3():
     x = np.linspace(-np.pi, np.pi)
     y = 1/x
     z = np.sin(x)
     fig = plt.figure()
-    ax = fig.add_subplot(111,projection="3d")
-    ax.plot(x, y, z, label="parametric curve")
-    plt.show()
+    ax = fig.add_subplot(111,projection = "3d")
+    ax.plot(x, y, z, label = "parametric curve")
+   # plt.show()
 
 
 def additional_task(TimeGif):
@@ -56,10 +57,10 @@ def additional_task(TimeGif):
     fig, ax = plt.subplots()
     x = np.arange(0, 2 * np.pi, 0.01)
     line, = ax.plot(x, np.sin(x))
-    ani = FuncAnimation(fig, update, frames=50*TimeGif, blit=True)
-    writer = PillowWriter(fps=50)
-    #plt.show()
-    ani.save("y=sin(x).gif", writer=writer)
+    ani = FuncAnimation(fig, update, frames = 50 * TimeGif, blit = True)
+    writer = PillowWriter(fps = 50)
+    plt.show()
+    ani.save("y=sin(x).gif", writer = writer)
 
 if __name__ == "__main__":
     task1()
@@ -67,6 +68,6 @@ if __name__ == "__main__":
     task3()
     print("Пожалуйста, не закрывайте программу!")
     print("Идёт запись гифки!")
-    TimeGif=3
+    TimeGif = 3
     additional_task(TimeGif)
     print("Гифка сохранена")
